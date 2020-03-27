f0de842a5c9ae8ce49665a224fc3c38d077791e2
James Jahraus <james.jahraus@gmail.com>
2020-03-27 00:58:43 +0000

    Client-Side Analysis

--- script.js
+++ script.js
@@ -64,20 +64,65 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
   layers.appendChild(link);
 }
 
-// Add buildings popup
+// Add buildings popup, and find closest food store to this building.
 map.on('click', 'denver-buildings', function (e) {
   map.getCanvas().style.cursor = 'crosshair';
   var buildingType = e.features[0].properties.type;
   var buildingID = e.features[0].properties.id;
 
+  // Find selected buildings turf point coordinates.
+  var coordinates = [e.lngLat.lng, e.lngLat.lat]
+  console.log("The coordinates:")
+  console.log(coordinates)
+
+  // Query all rendered features from a single layer
+  var features = map.queryRenderedFeatures({ layers: ['denver-food-stores'] });
+  console.log("The features:")
+  console.log(features)
+
+  // Create the feature collection from the features.
+  var stores = turf.featureCollection(features);
+  console.log("The stores:")
+  console.log(stores)
+
+  // Find the nearest store.
+  var nearestStore = turf.nearestPoint(coordinates, stores);
+  console.log("The nearest store:")
+  console.log(nearestStore)
+
+  // Find the stores coordinates.
+  var coordStore = turf.getCoord(nearestStore);
+  console.log("The nearest store's coordinates:")
+  console.log(coordStore)
+
+  // Find the store's LngLat.
+  var lnglatStore = mapboxgl.LngLat.convert(coordStore);
+  console.log("The nearest store's LngLat:")
+  console.log(lnglatStore)
+
+  // Find distance to store in meters.
+  var options = { units: 'meters' };
+  var distance = turf.distance(coordinates, coordStore, options);
+
+  // Create a popup for building.
   var popupHTML = `<p>Building ID: ${buildingID} <br> Building Type: ${buildingType}</p>`;
 
   new mapboxgl.Popup()
     .setLngLat(e.lngLat)
     .setHTML(popupHTML)
     .addTo(map);
+
+  // Create a popup for nearest store.
+  var storeHTML = `<p>Nearest Store: ${coordStore} <br> Distance: ${distance} meters</p>`;
+
+  new mapboxgl.Popup()
+    .setLngLat(lnglatStore)
+    .setHTML(storeHTML)
+    .addTo(map);
 })
 
+
+// Reset cursor on zoom
 map.on('zoom', function () {
   map.getCanvas().style.cursor = '';
 })
